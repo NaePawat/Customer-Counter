e27b69502c7e99165a1768a1a51c4e16a6775388
NaePawat <57982217+NaePawat@users.noreply.github.com>
2021-02-24 21:10:41 +0700

    Version 1.0
    +multiple capture from single object fixed
    +direction calculator (beta mak2)
    +counter exit/enter

--- objectDetector.py
+++ objectDetector.py
@@ -15,7 +15,14 @@ cap.set(4,720)
 cap.set(10,70)
 
 classNames= []
-person_id = []
+person_dict = dict()
+
+tempCoor = dict()
+
+boundary1 = 480
+boundary2 = 720
+ex_boundary1 = 240
+ex_boundary2 = 960
 
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
@@ -36,26 +43,56 @@ def checkLineCrossing(x,coorXLine):
         return 1
     else:
         return 0
+def checkZoneCrossing(x,y,direction):
+    bound1Distance = abs(x-boundary1)
+    bound2Distance = abs(x-boundary2)
+    if bound1Distance<bound2Distance and direction == 1:
+        return 1 #person enter from boundary 1
+    if bound2Distance<bound1Distance and direction == 0:
+        return 0 #person enter from boundary 2
 def checkLeavingImg(x):
     if x <= 1 or x>=720:
         return 1
     else:
         return 0
-def setId(idName):
-    person_id.append(idName)
-def removeId(idName):
-    if idName in person_id:
-        person_id.remove(idName)
-
+# def setId(idName):
+#     person_id.append(idName)
+# def settempDatabase(uniq_id,direction,boundary):
+#     person_info.insert(uniq_id,[direction,boundary])
+# def removeId(idName):
+#     if idName in person_id:
+#         person_id.remove(idName)
+def checkDirection(prev_x,cur_x,prev_y,cur_y):
+    #print("prev_x"+str(prev_x)+" , cur_x"+str(cur_x))
+    deltaX = cur_x-prev_x
+    deltaY = cur_y-prev_y
+    if deltaX > 0:
+        return 1 #walk to right
+    if deltaX < 0:
+        return 0 #walk to left
 while True:
-    success,img = cap.read()
-    cv2.line(img,(offXRef,0),(offXRef,720),(255,0,0),thickness=2)
 
-    classIds, confs, bbox = net.detect(img,confThreshold=threshold)
+    #fps calculation
+    timer = cv2.getTickCount()
+    success,img = cap.read()
+    fps = cv2.getTickFrequency()/(cv2.getTickCount()-timer)
+    cv2.putText(img,str(int(fps)),(960,50),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
+
+    #detect boundary
+    #cv2.line(img,(offXRef,0),(offXRef,720),(255,0,0),thickness=2)
+    cv2.rectangle(img,(boundary1,0),(boundary2,720),color = (255,255,0),thickness=2)
+
+    #exit boundary
+    cv2.line(img,(ex_boundary1,0),(ex_boundary1,720),(255,0,0),thickness=2)
+    cv2.line(img,(ex_boundary2,0),(ex_boundary2,720),(255,0,0),thickness=2)
+
+    try:
+        classIds, confs, bbox = net.detect(img,confThreshold=threshold)
+    except Exception as e:
+        print(str(e))
     bbox = list(bbox) #numpy array --> list
     confs = list(np.array(confs).reshape(1,-1)[0])
     confs = list(map(float,confs))
-    #print(classIds,bbox)
 
     indices = cv2.dnn.NMSBoxes(bbox,confs,threshold,nms_threshold) # non maximum suppression
 
@@ -66,26 +103,62 @@ while True:
             x,y,w,h = box[0],box[1],box[2],box[3]
             coorXCentroid = int((x+x+w)/2)
             coorYCentroid = int((y+y+h)/2)
-            #print(coorXCentroid)
             personCentroid = (coorXCentroid,coorYCentroid)
             cv2.circle(img,personCentroid,3,color=(0,0,255),thickness=5)
-            if (checkLeavingImg(coorXCentroid) and len(person_id)>0):
-                removeId(i)
-            if (checkLineCrossing(coorXCentroid,offXRef)):
-                if i in person_id:
-                    continue
-                else:
-                    enterCount += 1
-                    setId(i)
-                    print(person_id)
-
             cv2.rectangle(img,(x,y),(x+w,y+h),color=(0,255,0),thickness=2)
             cv2.putText(img,classNames[classIds[i][0]-1].upper(),(box[0]+10,box[1]+30),
                         cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
             cv2.putText(img,"Unique id: {}".format(str(i)).upper(),(box[0]+10,box[1]+60),
-                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-            cv2.putText(img, "Entrances: {}".format(str(enterCount)), (10, 50),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
+                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2) 
+            
+            #direction generator (beta mak2) len(tempCoor)>0
+            if i in tempCoor.keys():
+                #coorXCentroid_prev = tempCoor[i][0]
+                #coorYCentroid_prev = tempCoor[i][1]
+                coorCentroid_prev = tempCoor.get(i)
+                tempCoor.pop(i)
+            else:
+                coorCentroid_prev = [0,0]
+                #coorXCentroid_prev,coorYCentroid_prev=0,0
+
+            direction = checkDirection(coorCentroid_prev[0],coorXCentroid,coorCentroid_prev[1],coorYCentroid)
+            #set previous coordinate
+            coorXCentroid_prev = coorXCentroid
+            coorYCentroid_prev = coorYCentroid
+            #tempCoor.insert(i,[coorXCentroid_prev,coorYCentroid_prev])
+            tempCoor[i] = [coorXCentroid_prev,coorYCentroid_prev]
+
+            #data calculation
+            if boundary1<coorXCentroid<boundary2:
+                if i in person_dict.keys():
+                    continue
+                else:
+                    enter = checkZoneCrossing(coorXCentroid,coorYCentroid,i)
+                    person_dict[i] = enter
+            if i in person_dict.keys():
+                if coorXCentroid<ex_boundary1 and person_dict.get(i) == 0:
+                    person_dict.pop(i)
+                    exitCount += 1
+                elif coorXCentroid>ex_boundary1 and person_dict.get(i) == 1:
+                    person_dict.pop(i)
+                    enterCount += 1
+                else:
+                    continue
+            # if (checkLineCrossing(coorXCentroid,offXRef)):
+            #     if i in person_id:
+            #         continue
+            #     else:
+            #         enterCount += 1
+            #         setId(i)
+    print(person_dict)
+    cv2.putText(img, "Entrances: {}".format(str(enterCount)), (10, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 0, 1), 2)
+    cv2.putText(img, "Exit: {}".format(str(exitCount)), (10, 80),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 0, 1), 2)
+    cv2.imshow("Output",img)
+    cv2.waitKey(1)
+
+
     # if len(classIds) != 0: #have things to detect
     #     for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
     #         if classId == 1: #person
@@ -94,5 +167,20 @@ while True:
     #                         cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
     #             cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
     #                         cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-    cv2.imshow("Output",img)
-    cv2.waitKey(1)
+
+    # if boundary1<x<boundary2:
+    #             if i in person_id:
+    #                 break
+    #             else:
+    #                 enter = checkZoneCrossing(coorXCentroid,coorYCentroid)
+    #                 setId(i)
+    #                 settempDatabase(i,direction,enter)
+
+    # if (checkLeavingImg(coorXCentroid) and len(person_id)>0 and len(person_info)>0):
+    #             print("people out/in")
+    #             if coorXCentroid <= 1 and person_info[i][1]==0:
+    #                 print(exitCount)
+    #                 exitCount += 1
+    #             if coorXCentroid >= 720 and person_info[i][1]==0:
+    #                 enterCount += 1
+    #             removeId(i)
